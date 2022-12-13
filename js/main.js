9b3a574963ab61d40c5e8c22eb4e4275e7d0fc0f
asmaa100 <asmaaebrahim11298@gmail.com>
2022-12-13 16:04:24 +0200

    End home,auth

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var subMenu = document.getElementById('subMenu')
-var fullPageMenu = document.getElementById('fullPageMenu')
-subMenu.addEventListener('click', function () {
-  if (subMenu.className === 'menuClicked') {
-    subMenu.className = ""
-    setTimeout(function () { fullPageMenu.className = "visually-hidden" }, 200)
-  } else {
-    subMenu.className = 'menuClicked'
-    fullPageMenu.className = "slideRight"
-  }
-})
+
+window.onload = function(e){ 
+  let subMenu = document.getElementById('subMenu')
+  let fullPageMenu = document.getElementById('fullPageMenu')
+  subMenu.addEventListener('click', function () {
+    if (subMenu.className === 'menuClicked') {
+      subMenu.className = ""
+      setTimeout(function () { fullPageMenu.className = "visually-hidden" }, 200)
+    } else {
+      subMenu.className = 'menuClicked'
+      fullPageMenu.className = "slideRight"
+    }
+  })
+  
+}
+$('.togglePassword').click( function (e) {
+  // toggle the type attribute
+  let inputPassword = $(e.target).closest(".input-box").find(".id_password")
+  const type = inputPassword.attr("type") === 'password' ? 'text' : 'password';
+  inputPassword.attr("type", type)
+  e.target.classList.toggle('fa-eye');
+});
